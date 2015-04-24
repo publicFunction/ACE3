6e89614484ec8222630330bba2601e9a7396da49
jaynus <jaynus@gmail.com>
2015-04-24 09:59:37 -0700

    models and skeletons build and parse.

--- extensions/common/p3d/model.cpp
+++ extensions/common/p3d/model.cpp
@@ -1,14 +1,30 @@
 #include "p3d/model.hpp"
 
+#include "read_helpers.hpp"
+
 namespace ace {
     namespace p3d {
-        model::model() {
-            
+        model::model() : skeleton(nullptr) {}
+        model::model(std::fstream &stream_) {
+            model();
+
+            // get the full file size
+            stream_.seekg(0, std::ios::end);
+            size = stream_.tellg();
+            stream_.seekg(0, std::ios::beg);
+
             // Parse header here
-            
+            READ_DATA(filetype, sizeof(uint32_t));
+            READ_DATA(version, sizeof(uint64_t));
+            if (version >= 48) {
+                READ_STRING(prefix_name);
+            }
+
+            READ_DATA(lod_count, sizeof(uint32_t));
 
             // parse model info here
-
+            info = model_info(stream_, lod_count);
+            skeleton = &info.skeleton;
 
             // Read ahead because i dunno
             while (true) {
@@ -19,6 +35,9 @@ namespace ace {
                     break;
                 };
             }
+
+
+
         }
         model::~model() {}
     }
