913e33adeb5a711d97bc9609280da8593aef8955
jaynus <jaynus@gmail.com>
2015-04-26 09:19:05 -0700

    im dumb

--- extensions/common/transform_matrix.hpp
+++ extensions/common/transform_matrix.hpp
@@ -7,21 +7,22 @@ namespace ace {
     template <typename T>
     class transform_matrix_base {
     public:
-        transform_matrix_base<T> & operator= (const transform_matrix_base<T>& other) { _x = other.x(); _y = other.y(); _z = other.z(); _n = other.n();  return *this; }
+        transform_matrix_base & operator= (const transform_matrix_base& other) { _x = other.x(); _y = other.y(); _z = other.z(); _n = other.n();  return *this; }
 
         transform_matrix_base() {}
         transform_matrix_base(std::istream &stream_) : _x(stream_), _y(stream_), _z(stream_), _n(stream_) { }
         transform_matrix_base(ace::vector3<T> x_, ace::vector3<T> y_, ace::vector3<T> z_, ace::vector3<T> n_) : _x(x_), _y(y_), _z(z_), _n(n_) { }
 
-        const T & x() const { return _x; }
-        const T & y() const { return _y; }
-        const T & z() const { return _z; }
-        const T & n() const { return _n; }
+        const ace::vector3<T> & x() const { return _x; }
+        const ace::vector3<T> & y() const { return _y; }
+        const ace::vector3<T> & z() const { return _z; }
+        const ace::vector3<T> & n() const { return _n; }
 
-        void x(const T val) { _x = val; }
-        void y(const T val) { _y = val; }
-        void z(const T val) { _z = val; }
-        void n(const T val) { _n = val; }
+        void x(const ace::vector3<T> val) { _x = val; }
+        void y(const ace::vector3<T> val) { _y = val; }
+        void z(const ace::vector3<T> val) { _z = val; }
+        void n(const ace::vector3<T> val) { _n = val; }
+
     protected:
         ace::vector3<T> _x;
         ace::vector3<T> _y;
