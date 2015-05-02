1aafb2e191db38f40e36e931286b7bd332bb8a70
jaynus <jaynus@gmail.com>
2015-05-01 19:32:23 -0700

    Development dynamic loader. Dynamically will unload/load a DLL for test usage as an extension.

--- extensions/dynload/dynloader.hpp
+++ extensions/dynload/dynloader.hpp
@@ -7,29 +7,63 @@
 typedef void (*__stdcall RVExtension)(char *output, int outputSize, const char *function);
 
 namespace ace {
+
     class module {
     public:
-        module() : handle(INVALID_HANDLE_VALUE), function(nullptr), name("") {}
-        module(std::string name_, HANDLE handle_, RVExtension function_) : handle(handle_), function(function_), name(name_) {}
+        module() : handle(nullptr), function(nullptr), name("") {}
+        module(std::string name_, HANDLE handle_, RVExtension function_) : handle(nullptr), function(function_), name(name_) {}
 
         std::string name;
-        HANDLE      handle;
+        HINSTANCE   handle;
         RVExtension function;
     };
 
     class dynloader : public singleton<dynloader> {
     public:
+        dynloader() {}
+        ~dynloader() {
+            for (auto & kv : _modules) {
+                arguments temp(kv.first);
+                std::string result_temp;
+                unload(temp, result_temp);
+            }
+        }
+
+#ifdef _WINDOWS
         bool load(const arguments & args_, std::string & result) {
-            
-            
+            HINSTANCE dllHandle;
+            RVExtension function;
+
+            if (_modules.find(args_.as_string(0)) != _modules.end()) {
+                return true;
+            }
+
+            dllHandle = LoadLibrary(args_.as_string(0).c_str());
+            if (!dllHandle)
+                return false;
+
+            function = (RVExtension)GetProcAddress(dllHandle, "RVExtension");
+            if (!function) {
+                FreeLibrary(dllHandle);
+            }
+
+            _modules[args_.as_string(0)] = module(args_.as_string(0), dllHandle, function);
 
             return false;
         }
         bool unload(const arguments & args_, std::string & result) {
 
+            if (_modules.find(args_.as_string(0)) != _modules.end()) {
+                return true;
+            }
+
+            FreeLibrary(_modules[args_.as_string(0)].handle);
+            _modules.erase(args_.as_string(0));
 
             return false;
         }
+#endif
+
         bool call(const arguments & args_, std::string & result) {
             if (_modules.find(args_.as_string(0)) == _modules.end()) {
                 return false;
@@ -37,9 +71,14 @@ namespace ace {
             std::string output, function_str;
             output.resize(4096);
 
-            function_str = args_.get();
-            
+            std::vector<std::string> temp = ace::split(args_.get(), ',');
+            int x = 0;
+            for (x = 1; x < temp.size(); x++)
+                function_str = function_str + temp[x] + ",";
+
             _modules[args_.as_string(0)].function((char *)output.c_str(), 4096, (const char *)function_str.c_str());
+
+            return true;
         }
         
         bool register_functions() {
