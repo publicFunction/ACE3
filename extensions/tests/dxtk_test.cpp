e124bda9bedccd0b9895bed5bbde37a9abad1443
jaynus <jaynus@gmail.com>
2015-05-05 13:34:21 -0700

    DirectX toolkit integration.

--- extensions/tests/dxtk_test.cpp
+++ extensions/tests/dxtk_test.cpp
@@ -2,10 +2,34 @@
 
 #include "directx/d3d_display.hpp"
 
+#include "CommonStates.h"
+#include "DDSTextureLoader.h"
+#include "Effects.h"
+#include "GeometricPrimitive.h"
+#include "Model.h"
+#include "PrimitiveBatch.h"
+#include "ScreenGrab.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+
 using namespace ace::debug;
+using namespace DirectX;
+
+class d3d_test : public d3d_display {   
+public:
+    bool step(void) override {
+
+        return true;
+    }
+	std::unique_ptr<CommonStates>                           _States;
+	std::unique_ptr<GeometricPrimitive>                     _Shape;
+	std::unique_ptr<PrimitiveBatch<VertexPositionColor>>    _Batch;
+	std::unique_ptr<SpriteFont>                             _Font;
+};
 
 int main(int argc, char **argv) {
-    d3d_display test;
+    d3d_test test;
     test.create();
     test.init();
 
