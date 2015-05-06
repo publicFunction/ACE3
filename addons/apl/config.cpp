ca4c2734d61493ba589adb8403e949a8f5e76b7e
jaynus <jaynus@gmail.com>
2015-05-06 07:17:46 -0700

    Fixes make.py breakage.

--- addons/apl/config.cpp
+++ addons/apl/config.cpp
@@ -1,21 +1,15 @@
-class CfgPatches
-{
-	class ace_APL
-	{
-		units[]={};
-		weapons[]={};
-		requiredVersion=0.5;
-		requiredAddons[]=
-		{
-			"ace_main"
-		};
-		author[]=
-		{
-			"Bohemia Interactive"
-		};
-		authorUrl="http://ace3mod.com";
-		version="3.0.0.2-e7c0b7e6";
-		versionStr="3.0.0.2-e7c0b7e6";
-		versionAr[]={3,0,0,2};
-	};
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"ace_main"};
+        author[] = {"Bohemia Interactive"};
+        authorUrl = "http://ace3mod.com";
+        VERSION_CONFIG;
+    };
 };
+
+#include "CfgEventHandlers.hpp"
